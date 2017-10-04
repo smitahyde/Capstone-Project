b3c46e9518c35b72ac4fa20d855d459b09328a97
Smita Hyde <smitahyde@gmail.com>
2017-10-04 14:51:43 -0400

    update changes

--- main.js
+++ main.js
@@ -10,14 +10,14 @@ var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+    labels: ['<20', '20-34', '34-44', '45-54', '55-64', '65-74', '75-84','>84'],
     datasets: [{
       label: 'Men',
-      data: [5, 10, 3, 7, 6, 3, 7],
+      data: [0, 5, 10, 15, 20, 25, 30,35,40],
       backgroundColor: "rgba(153,255,51,0.4)"
     }, {
       label: 'Women',
-      data: [2, 7, 5, 5, 2, 3, 10],
+      data: [0, 5, 10, 15, 20, 25, 30,35,40],
       backgroundColor: "rgba(255,153,0,0.4)"
     }]
   }
